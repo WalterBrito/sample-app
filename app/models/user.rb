2659b24b8d43449063299f9022b13de6186452d9
WalterBrito <waltter.britto@gmail.com>
2016-11-08 22:35:51 -0300

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_acessor :remember_token
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -29,6 +29,12 @@ class User < ApplicationRecord
  
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
